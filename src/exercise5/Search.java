f2525e2bef3026a4ace3363fafcc44e2bb9e2788
EDDYtu22 <201221004@fdiba.tu-sofia.bg>
2021-11-11 00:07:19 +0200

    Ubung 5

--- src/exercise5/Search.java
+++ src/exercise5/Search.java
@@ -1,27 +1,34 @@
 package exercise5;
-
 import java.util.Scanner;
 
 public class Search {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int i;
+        int sCounter = 0;
 
-        System.out.print("Enter number count: ");
-        int numberCount = input.nextInt();
+        //System.out.print("Enter the number of elements: ");
+        int size = input.nextInt();
 
-        int[] numbers = new int[numberCount];
+        int numbers[] = new int[size];
 
-        System.out.println("Enter numbers: ");
-        for (int index = 0; index < numbers.length; index++) {
-            numbers[index] = input.nextInt();
+        for (i = 0; i < numbers.length; i++) {
+            //System.out.print("Enter value for " + i + " element: ");
+            numbers[i] = input.nextInt();
         }
+        //System.out.print("Witch element are you searching for? ");
+        int searchedNumber = input.nextInt();
 
-        System.out.print("Search for number: ");
-        int number = input.nextInt();
 
-        int numberIndex = -1;
-        // Search for number in numbers
-
-        System.out.println("Number index: " + numberIndex);
+        for ( i = 0; i < numbers.length; i++) {
+            if (searchedNumber == numbers[i]) {
+                System.out.println("Number index: " + i);
+                break;
+            }
+            sCounter++;
+            if (sCounter == numbers.length) {
+                System.out.println("Number index: -1 ");
+            }
+        }
     }
 }
