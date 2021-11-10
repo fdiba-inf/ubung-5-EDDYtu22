0156313799cfb3c1c11c209bcd5f77419dfa08d0
EDDYtu22 <201221004@fdiba.tu-sofia.bg>
2021-11-10 23:55:33 +0200

    Ubung 5

--- src/Search.java
+++ src/Search.java
@@ -5,29 +5,31 @@ public class Search {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int i;
+        int sCounter = 0;
 
-        //System.out.print("Enter the number of elements: ");
+        System.out.print("Enter the number of elements: ");
         int size = input.nextInt();
 
         int numbers[] = new int[size];
 
         for (i = 0; i < numbers.length; i++) {
-            //System.out.print("Enter value for " + i + " element: ");
+            System.out.print("Enter value for " + i + " element: ");
             numbers[i] = input.nextInt();
         }
-        //System.out.print("Witch element are you searching for? ");
+        System.out.print("Witch element are you searching for? ");
         int searchedNumber = input.nextInt();
 
 
         for ( i = 0; i < numbers.length; i++) {
             if (searchedNumber == numbers[i]) {
                 System.out.println("Number index: " + i);
-            } else {
-                System.out.println("Number index: -1");
                 break;
             }
+            sCounter++;
+            if (sCounter == numbers.length) {
+                System.out.println("Number index: -1 ");
+            }
         }
-
-
     }
 }
+
